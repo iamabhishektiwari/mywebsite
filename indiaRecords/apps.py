a44c826c4cfcd89d949ddb8e44f08f0d3bd4c0ad
iamabhishektiwari <abhishek.tiwari.cer15@itbhu.ac.in>
2020-04-20 07:23:16 +0530

    some pythonanywhere issue

--- indiaRecords/apps.py
+++ indiaRecords/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class IndiarecordsConfig(AppConfig):
     name = 'indiaRecords'
 
-    def ready(self):
-        from dbUpdater import updater
-        updater.start()
+    # def ready(self):
+    #     from dbUpdater import updater
+    #     updater.start()
